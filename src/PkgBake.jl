7fdf94c02785273f3fc7a7e0c977372942f6ba5d
Steve Kelly <kd2cca@gmail.com>
2020-05-27 17:43:56 -0400

    somewhat working load, sanitizer need to hand Base submodules better

--- src/PkgBake.jl
+++ src/PkgBake.jl
@@ -25,11 +25,71 @@ const bakeable_libs = [Base,
 
 Add additional precompiled methods to Base and StdLibs that are self contained.
 """
-function bake(;dir="", replace_default=true)
-    st = Pkg.installed()
-    @info "PkgBake: Loading Packages and generating precompile statments"
-    ctx = isempty(dir) ? create_pkg_context(Base.active_project()) : create_pkg_context(dir)
-    @show Pkg.Operations.load_manifest_deps(ctx)
+function bake(;dir=dirname(Base.active_project()), replace_default=true)
+    @info "PkgBake: Loading Packages and generating precompile statements for $dir"
+    ctx = create_pkg_context(dir)
+    deps = values(Pkg.dependencies(create_pkg_context(dir)))
+
+    pipe_out = "pipeout.txt"
+    touch(pipe_out)
+
+    precompile_lines = String[]
+
+    progress = Progress(length(deps), 1)
+    for dep in deps
+
+        next!(progress, showvalues = [(:dep,dep.name), (:statements, length(precompile_lines))])
+
+        # skip stdlibs
+        if in(dep.name, string.(bakeable_libs)) || !dep.is_direct_dep
+            continue
+        end
+        pc_temp = tempname()
+        touch(pc_temp)
+        cmd = `$(get_julia_cmd()) --project=$dir --trace-compile=$pc_temp -e $("using $(dep.name)")`
+        try
+            run(pipeline(cmd, stdout=pipe_out, stderr=pipe_out, append=true))
+        catch err
+            if isa(err, InterruptException)
+                @warn "PkgBake: Interrupted by user"
+                exit()
+            else
+                continue
+            end
+        end
+        new_lines = readlines(pc_temp)
+        rm(pc_temp)
+        if !isempty(new_lines)
+            append!(precompile_lines, new_lines)
+        end
+    end
+
+    unique!(sort!(precompile_lines))
+
+    timestamp = Dates.format(now(), "yyyy-mm-ddTHH_MM")
+    pc_unsanitized = "pkgbake_unsanitized_$(timestamp).jl"
+    @info "PkgBake: Writing unsanitized precompiles to $pc_unsanitized"
+    open(pc_unsanitized, "w") do io
+        for line in precompile_lines
+            println(io, line)
+        end
+    end
+
+    original_len = length(precompile_lines)
+    @info "PkgBake: Santizing precompile statments"
+    sanitized_lines = sanitize_precompile(precompile_lines)
+    sanitized_len = length(sanitized_lines)
+
+    pc_sanitized = "pkgbake_sanitized_$(timestamp).jl"
+    @info "PkgBake: Writing unsanitized precompiles to $pc_sanitized"
+    open(pc_sanitized, "w") do io
+        for line in sanitized_lines
+            println(io, line)
+        end
+    end
+
+    @info "PkgBake: Found $sanitized_len precompilable methods for base out of $original_len generated statements"
+    #@show precompile_lines, sanitized_lines
     #PackageCompiler.create_sysimage(; precompile_statements_file="ohmyrepl_precompile.jl", replace_default=replace_default)
 end
 
@@ -39,9 +99,9 @@ end
 Prepares and sanitizes a julia file for precompilation. This removes any non-concrete
 methods and anything non-Base or StdLib.
 """
-function sanitize_precompile(path::String)
+function sanitize_precompile(precompile_lines::Vector{String})
     lines = String[]
-    for line in readlines(path)
+    for line in precompile_lines
         # Generally any line with where is non-concrete, so we can skip.
         # Symbol is also runtime dependent so skip as well
         if isempty(line) || contains(line, "where") || contains(line, "Symbol(")
@@ -76,9 +136,8 @@ end
     recurse through the call and make sure everything is in Base, Core, or a StdLib
 """
 function is_bakeable(ex::Expr)
-    @show ex, typeof(ex)
     for arg in ex.args
-        @show arg, typeof(arg)
+        #@show arg, typeof(arg)
         if is_bakeable(arg)
             continue
         else
@@ -89,10 +148,11 @@ function is_bakeable(ex::Expr)
 end
 
 function is_bakeable(ex::Symbol)
-    @show ex, typeof(ex)
     for lib in bakeable_libs
         if isdefined(lib, ex)
             return true
+        else
+            continue
         end
     end
     return false
